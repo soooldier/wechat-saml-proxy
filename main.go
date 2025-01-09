f93a7edfac6fe8bc7ebf5d1c384cab72e4afa4ab
yitttang <yitttang@tencent.com>
2025-01-09 17:07:18 +0800

    fixed

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/memstore"
+	"github.com/gin-gonic/gin"
+	"wechat-saml-proxy/service"
+)
+
+func main() {
+	r := gin.Default()
+	store := memstore.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+	r.GET("/", func(c *gin.Context) {
+		b, err := ioutil.ReadFile("./index.html")
+		if err != nil {
+			c.HTML(500, "Internal Error", nil)
+			c.Abort()
+		}
+		c.HTML(200, string(b), nil)
+	})
+	r.GET("/MP_verify_32iWga2EVle6QTQm.txt", func(c *gin.Context) {
+		c.HTML(200, string("32iWga2EVle6QTQm"), nil)
+	})
+	r.GET("/api/callback", service.LoginHandler)
+	r.GET("/api/saml", func(c *gin.Context) {
+		openid4interface := sessions.Default(c).Get("openid4wechat")
+		openid4string := openid4interface.(string)
+		c.HTML(200, openid4string, nil)
+	})
+	r.Run(":80")
+}
---
+++ service/login_service.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"encoding/json"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/silenceper/wechat/v2"
+	"github.com/silenceper/wechat/v2/cache"
+	"github.com/silenceper/wechat/v2/officialaccount/config"
+	"github.com/silenceper/wechat/v2/officialaccount/oauth"
+)
+
+var (
+	appId     = "wxbb7b02e8aaffb2e4"
+	appSecret = "b81d6464d1a8576b44fcf7f4452795b4"
+	token     = "TOKEN"
+)
+
+// LoginHandler 微信登录
+func LoginHandler(c *gin.Context) {
+	code := c.Query("code")
+	user, err := getWechatOfficialNickname(appId, appSecret, token, code, cache.NewMemory())
+	if err != nil {
+		c.HTML(500, err.Error(), nil)
+		c.Abort()
+	}
+	user4json, err := json.Marshal(user)
+	if err != nil {
+		c.HTML(500, err.Error(), nil)
+		c.Abort()
+	}
+	session := sessions.Default(c)
+	session.Set("openid4wechat", user.OpenID)
+	session.Save()
+	c.HTML(200, string(user4json), nil)
+}
+
+func getWechatOfficialNickname(appId, appSecret, token, code string, cache cache.Cache) (userinfo oauth.UserInfo, err error) {
+	cfg := &config.Config{
+		AppID:     appId,
+		AppSecret: appSecret,
+		Token:     token,
+		Cache:     cache,
+	}
+	wc := wechat.NewWechat()
+	officialAccount := wc.GetOfficialAccount(cfg)
+	oauth := officialAccount.GetOauth()
+	accessToken, err := oauth.GetUserAccessToken(code)
+	if err != nil {
+		return
+	}
+	userinfo, err = oauth.GetUserInfo(accessToken.AccessToken, accessToken.OpenID, "")
+	if err != nil {
+		return
+	}
+	return
+}
