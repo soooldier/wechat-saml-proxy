fc737e1b5cbc8bd98ac4bda555f58c097063e819
soooldier <soooldier@live.com>
2025-01-10 08:16:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -37,9 +37,6 @@ func main() {
 	http.HandleFunc("/MP_verify_32iWga2EVle6QTQm.txt", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "32iWga2EVle6QTQm")
 	})
-	http.HandleFunc("/Cmc9dbMeWY.txt", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "8f8803127815d7155df916cacfcaa747")
-	})
 	http.HandleFunc("/F3zUTYqMMi.txt", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "44ae53897a43b86e1e83d6aaae33addd")
 	})
